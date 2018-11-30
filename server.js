7c2c549040405af036792ef8d0a89b5acd6039b8
David Lee <leed13@wit.edu>
2018-11-30 00:30:21 -0500

    changes to port for heroku setup

--- server.js
+++ server.js
@@ -38,4 +38,4 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 //image url endpoint to mask Clarifai API call and response activity on the backend/server
 app.post('/imageurl', (req, res) => {image.handleAPICall(req, res)});
 
-app.listen(3000, () =>{console.log('app is running on port 3000')});
+app.listen(process.env.PORT || 3000, () =>{console.log(`app is running on port ${process.env.PORT}`)});
