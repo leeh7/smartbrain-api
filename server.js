2d49ef01fc491cc5bdcebfc49bd9e3e23cf54a49
David Lee <leed13@wit.edu>
2018-11-30 01:03:27 -0500

    attaching postgresql db on heroku to server 2

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-slippery-54423',
-      user : '',
-      password : '',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
 });
 
